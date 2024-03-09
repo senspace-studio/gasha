09c973e6aacc6f7ccbd3f8ea099c875b9d536176
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-03-09 12:02:28 +0900

    init

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,36 @@
+import Model from '@/components/models/File'
+import { Box, Container } from '@chakra-ui/react'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+
+export default function Home() {
+  return (
+    <Container>
+      <Box
+        margin="0 auto"
+        backgroundImage="url('/img/item_bg.png')"
+        w={500}
+        h={500}
+        backgroundSize="cover"
+      >
+        <Canvas style={{ width: 500, height: 500 }}>
+          <Suspense fallback={null}>
+            <Model />
+          </Suspense>
+          <PerspectiveCamera makeDefault position={[0, 0, 1]} />
+          <OrbitControls makeDefault />
+          <ambientLight intensity={1} />
+          <hemisphereLight intensity={1} />
+          <spotLight
+            position={[0, 0, 1.5]}
+            intensity={1}
+            angle={0.3}
+            penumbra={0.5}
+            castShadow
+          />
+        </Canvas>
+      </Box>
+    </Container>
+  )
+}
