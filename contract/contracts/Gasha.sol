c3b3db8603e9684309a41dcd2ac730a2996a8626
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-03-18 09:00:11 +0900

    upgradeable gasha

--- contract/contracts/Gasha.sol
+++ contract/contracts/Gasha.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
-import "@zoralabs/zora-1155-contracts/src/interfaces/IZoraCreator1155.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./zora/interfaces/IZoraCreator1155.sol";
 import "./interfaces/IGasha.sol";
 import "hardhat/console.sol";
 
-contract Gasha is IGasha {
+contract Gasha is IGasha, OwnableUpgradeable {
     IZoraCreator1155 public ZoraCreator1155;
 
     IMinter1155 public MerkleMinter;
@@ -20,17 +21,19 @@ contract Gasha is IGasha {
 
     uint256 public unitPrice;
 
-    constructor(
+    function initialize(
+        address _initialOwner,
         address _zoraCreator1155,
         address _merkleMinter,
         address _mintReferral,
-        uint256 initialSeed,
+        uint256 _initialSeed,
         uint256 _unitPrice
-    ) {
+    ) public initializer {
+        __Ownable_init(_initialOwner);
         ZoraCreator1155 = IZoraCreator1155(_zoraCreator1155);
         MerkleMinter = IMinter1155(_merkleMinter);
         mintReferral = _mintReferral;
-        seed = initialSeed;
+        seed = _initialSeed;
         unitPrice = _unitPrice;
     }
 
@@ -84,8 +87,9 @@ contract Gasha is IGasha {
         uint256 salt
     ) internal view returns (SeriesItem memory item) {
         uint256 totalWeight = 0;
-        for (uint256 i = 0; i < series.length; i++) {
-            totalWeight += series[i].weight;
+        SeriesItem[] memory seriesItem = activeSeriesItems();
+        for (uint256 i = 0; i < seriesItem.length; i++) {
+            totalWeight += seriesItem[i].weight;
         }
 
         uint256 randomNum = uint256(
@@ -95,14 +99,38 @@ contract Gasha is IGasha {
         ) % totalWeight;
 
         uint256 sum = 0;
-        for (uint256 i = 0; i < series.length; i++) {
-            sum += series[i].weight;
+        for (uint256 i = 0; i < seriesItem.length; i++) {
+            sum += seriesItem[i].weight;
             if (randomNum < sum) {
-                return series[i];
+                return seriesItem[i];
             }
         }
 
         revert("Gasha: failed to pick a random ball");
+    }
+
+    function activeSeriesItems() public view returns (SeriesItem[] memory) {
+        uint256 activeItemCount = 0;
+        for (uint256 i = 0; i < series.length; i++) {
+            if (series[i].isActive) {
+                activeItemCount++;
+            }
+        }
+
+        SeriesItem[] memory activeItems = new SeriesItem[](activeItemCount);
+        uint256 currentIndex = 0;
+        for (uint256 i = 0; i < series.length; i++) {
+            if (series[i].isActive) {
+                activeItems[currentIndex] = series[i];
+                currentIndex++;
+            }
+        }
+
+        return activeItems;
+    }
+
+    function seriesItems() public view returns (SeriesItem[] memory) {
+        return series;
     }
 
     // todo: onlyOwner
@@ -110,25 +138,39 @@ contract Gasha is IGasha {
         uint256 tokenId,
         Rareness rareness,
         uint256 weight
-    ) public {
-        series.push(SeriesItem(tokenId, rareness, weight));
+    ) public onlyOwner {
+        series.push(SeriesItem(tokenId, rareness, weight, true));
     }
 
     // todo: onlyOwner
-    function removeSeriesItem(uint256 tokenId) public {
+    function activateSeriesItem(uint256 tokenId) public onlyOwner {
         for (uint256 i = 0; i < series.length; i++) {
             if (series[i].tokenId == tokenId) {
-                for (uint256 j = i; j < series.length - 1; j++) {
-                    series[j] = series[j + 1];
-                }
-                series.pop();
+                series[i].isActive = true;
                 break;
             }
         }
     }
 
     // todo: onlyOwner
-    function setMinterArguments(bytes memory _minterArguments) external {
+    function deactivateSeriesItem(uint256 tokenId) public onlyOwner {
+        for (uint256 i = 0; i < series.length; i++) {
+            if (series[i].tokenId == tokenId) {
+                series[i].isActive = false;
+                break;
+            }
+        }
+    }
+
+    // todo: onlyOwner
+    function resetSeed(uint256 newSeed) public onlyOwner {
+        seed = newSeed;
+    }
+
+    // todo: onlyOwner
+    function setMinterArguments(
+        bytes memory _minterArguments
+    ) external onlyOwner {
         minterArguments = _minterArguments;
     }
 
