4660b58ff26af64e02a0e007c913fc6ebffffea4
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-03-21 20:07:39 +0900

    fix disconnect wallet

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,11 +3,14 @@ import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { PrivyProvider } from '@privy-io/react-auth'
 import { WagmiProvider } from '@privy-io/wagmi'
-import { wagmiConfig } from '@/lib/wagmi'
 import { DefaultLayout } from '@/components/layout/Layout'
 import { chakraTheme } from '@/lib/chakraTheme'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+import { useChain } from '@/hooks/useChain'
+import { wagmiConfig } from '@/lib/wagmi'
+
+const config = wagmiConfig()
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
@@ -15,7 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <PrivyProvider appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!} config={{}}>
       <QueryClientProvider client={queryClient}>
-        <WagmiProvider config={wagmiConfig()}>
+        <WagmiProvider config={config}>
           <ChakraProvider theme={chakraTheme}>
             <DefaultLayout>
               <Component {...pageProps} />
