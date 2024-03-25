ac84ae1f9140d36367b8498494dc47743518e7f4
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-03-26 00:49:47 +0900

    site url env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_VERCEL_URL: process.env.VERCEL_URL,
-  },
 }
 
 export default nextConfig
