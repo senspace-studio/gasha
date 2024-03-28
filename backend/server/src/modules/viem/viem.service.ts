33b78abe45854ce6a235ca8c0d465be012cb9b81
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-03-28 15:40:06 +0900

    tmp

--- backend/server/src/modules/viem/viem.service.ts
+++ backend/server/src/modules/viem/viem.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { BLOCKCHAIN_API, GASHA_ADDRESS } from 'src/utils/env';
-import { http, createPublicClient, Chain, Address, getContract } from 'viem';
+import {
+  http,
+  createPublicClient,
+  Chain,
+  Address,
+  getContract,
+  createWalletClient,
+} from 'viem';
+import { privateKeyToAccount } from 'viem/accounts';
 import { baseSepolia as chain, mainnet } from 'viem/chains';
-import { Gasha } from 'src/constants/Gasha';
+import { Gasha, GashaABI } from 'src/constants/Gasha';
 import { SeriesItem } from 'src/types/contract';
 
 @Injectable()
@@ -12,6 +20,17 @@ export class ViemService {
       chain: chain as Chain,
       transport: http(BLOCKCHAIN_API),
     });
+  }
+
+  private get walletClient() {
+    return createWalletClient({
+      chain: chain as Chain,
+      transport: http(BLOCKCHAIN_API),
+    });
+  }
+
+  private get adminAccount() {
+    return privateKeyToAccount(process.env.ADMIN_PRIVATE_KEY as Address);
   }
 
   private get ensResolverClient() {
@@ -81,5 +100,17 @@ export class ViemService {
     } catch (error) {
       throw new Error('Invalid address');
     }
+  }
+
+  async dropByAdmin(address: Address, tokenId: number) {
+    const { request } = await this.client.simulateContract({
+      address: GASHA_ADDRESS as Address,
+      abi: GashaABI,
+      account: this.adminAccount,
+      functionName: 'dropByOwner',
+      args: [address, [BigInt(tokenId)], [BigInt(1)]],
+    });
+
+    await this.walletClient.writeContract(request);
   }
 }
