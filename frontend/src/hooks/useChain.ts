b6af538295f413665ccdec648d271b1a21f04f47
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-03-28 12:08:56 +0900

    fix switch chain

--- frontend/src/hooks/useChain.ts
+++ frontend/src/hooks/useChain.ts
@@ -1,7 +1,30 @@
 import { wagmiConfig } from '@/lib/wagmi'
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
+import { toast } from 'react-toastify'
+import { useSwitchChain as wagmiSwitchChain } from 'wagmi'
 
 export const useChain = () => {
   const config = useMemo(() => wagmiConfig(), [])
   return config
+}
+
+export const useSwitchChain = () => {
+  const { switchChainAsync, status } = wagmiSwitchChain()
+
+  const switchChain = useCallback(async () => {
+    try {
+      await switchChainAsync({
+        chainId: Number(process.env.NEXT_PUBLIC_CHAIN_ID),
+      })
+      toast.info('Switched!')
+    } catch (error: any) {
+      if (error.message?.includes('already pending')) {
+        toast.info('Please check your wallet to switch network')
+      } else {
+        toast.error('Failed to switch network')
+      }
+    }
+  }, [switchChainAsync, status])
+
+  return { switchChain }
 }
