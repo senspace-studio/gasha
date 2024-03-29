e0203518de0b69221e1f1d95593547ff2f1f566e
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-03-29 22:15:15 +0900

    tmp

--- frontend/src/components/uiparts/ResultGallery.tsx
+++ frontend/src/components/uiparts/ResultGallery.tsx
@@ -88,7 +88,7 @@ export const ResultGallery: FC = () => {
             onInit={(ev) => {
               setSwiper(ev)
             }}
-            modules={[Autoplay]}
+            // modules={[Autoplay]}
           >
             {gotItems.map((item, index) => (
               <SwiperSlide key={index}>
