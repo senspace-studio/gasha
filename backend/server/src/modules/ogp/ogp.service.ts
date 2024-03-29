200bfae5d4ab5ec5e19c74ef3b68b6db992e34c7
yu23ki14 <yuki_021423@yahoo.co.jp>
2024-03-29 19:52:21 +0900

    ogp

--- backend/server/src/modules/ogp/ogp.service.ts
+++ backend/server/src/modules/ogp/ogp.service.ts
@@ -2,8 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { readFileSync } from 'fs';
 import * as sharp from 'sharp';
 import { join } from 'path';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ScorecardEntity } from 'src/entities/scorecard';
+import { Repository } from 'typeorm';
 
 const blue = '#0554F2';
+
+const fontMedium = join(__dirname, '../../assets/fonts/AlbertSans-Medium.ttf');
+const fontBold = join(__dirname, '../../assets/fonts/AlbertSans-ExtraBold.ttf');
+const fontBlack = join(__dirname, '../../assets/fonts/AlbertSans-Black.ttf');
 
 const createTextSVG = async (
   text: string,
@@ -41,44 +48,23 @@ const createTextSVG = async (
     .toBuffer();
 };
 
+type item = {
+  rareness: 'common' | 'rare' | 'special';
+  points: number;
+  tokens: {
+    tokenId: number;
+    quantity: number;
+  }[];
+};
+
 @Injectable()
 export class OgpService {
-  async generateSquareOgp() {
-    const totalPoint = 200;
+  constructor(
+    @InjectRepository(ScorecardEntity)
+    private readonly scorecardRepository: Repository<ScorecardEntity>,
+  ) {}
 
-    const items = [
-      {
-        rareness: 'special',
-        points: 100,
-        tokens: [
-          {
-            tokenId: 1,
-            quantity: 1,
-          },
-        ],
-      },
-      {
-        rareness: 'rare',
-        points: 100,
-        tokens: [
-          {
-            tokenId: 1,
-            quantity: 1,
-          },
-        ],
-      },
-      {
-        rareness: 'common',
-        points: 100,
-        tokens: [
-          {
-            tokenId: 1,
-            quantity: 1,
-          },
-        ],
-      },
-    ];
-
+  async generateSquareOgp(totalPoint: number, address: string, items: item[]) {
     const base = readFileSync(
       join(__dirname, '../../assets/images/ogp/square-base.png'),
     );
@@ -87,12 +73,12 @@ export class OgpService {
 
     const point = await sharp({
       text: {
-        text: `<span foreground="${blue}">${totalPoint}</span>`,
+        text: `<span foreground="${blue}" font_weight="bold">${totalPoint}</span>`,
         font: 'Albert Sans',
-        fontfile: join(__dirname, '../../assets/fonts/AlbertSans-Medium.ttf'),
+        fontfile: fontBold,
         rgba: true,
         width: 550,
-        height: 90,
+        height: 84,
       },
     })
       .png()
@@ -100,9 +86,9 @@ export class OgpService {
 
     const pointLabel = await sharp({
       text: {
-        text: `<span foreground="${blue}">$BALL</span>`,
+        text: `<span foreground="${blue}" font_weight="bold">$BALL</span>`,
         font: 'Albert Sans',
-        fontfile: join(__dirname, '../../assets/fonts/AlbertSans-Regular.ttf'),
+        fontfile: fontBold,
         rgba: true,
         width: 150,
         height: 34,
@@ -111,11 +97,13 @@ export class OgpService {
       .png()
       .toBuffer();
 
-    const address = await sharp({
+    const addressImage = await sharp({
       text: {
-        text: `<span foreground="black">0x12...678E</span>`,
+        text: `<span foreground="black">${
+          address.slice(0, 6) + '...' + address.slice(-4)
+        }</span>`,
         font: 'Albert Sans',
-        fontfile: join(__dirname, '../../assets/fonts/AlbertSans-Regular.ttf'),
+        fontfile: fontMedium,
         rgba: true,
         width: 550,
         height: 28,
@@ -132,8 +120,8 @@ export class OgpService {
         const ball = await sharp(ballImage).resize(72, 72).png().toBuffer();
         return {
           input: ball,
-          left: 50,
-          top: 610 + index * 130,
+          left: 51,
+          top: 616 + index * 125,
         };
       }),
     );
@@ -141,23 +129,20 @@ export class OgpService {
       items.map(async (item, index) => {
         const points = await sharp({
           text: {
-            text: `<span foreground="black">${item.points}</span>`,
+            text: `<span foreground="black" font_weight="bold">${item.points}</span>`,
             font: 'Albert Sans',
-            fontfile: join(
-              __dirname,
-              '../../assets/fonts/AlbertSans-Regular.ttf',
-            ),
+            fontfile: fontBold,
             rgba: true,
             width: 550,
-            height: 40,
+            height: 45,
           },
         })
           .png()
           .toBuffer();
         return {
           input: points,
-          left: 180,
-          top: 630 + index * 135,
+          left: 155,
+          top: 630 + index * 128,
         };
       }),
     );
@@ -165,23 +150,59 @@ export class OgpService {
       items.map(async (item, index) => {
         const points = await sharp({
           text: {
-            text: `<span foreground="black">$BALL</span>`,
+            text: `<span foreground="black" font_weight="bold">$BALL</span>`,
             font: 'Albert Sans',
-            fontfile: join(
-              __dirname,
-              '../../assets/fonts/AlbertSans-Regular.ttf',
-            ),
+            fontfile: fontBlack,
             rgba: true,
-            width: 550,
-            height: 20,
+            width: 100,
+            height: 23,
           },
         })
           .png()
           .toBuffer();
         return {
           input: points,
-          left: 340,
-          top: 650 + index * 135,
+          left: 180 + item.points.toString().length * 35,
+          top: 652 + index * 128,
+        };
+      }),
+    );
+
+    const itemsImageContainer = await Promise.all(
+      items.map(async (item, index) => {
+        const itemImage = readFileSync(
+          join(
+            __dirname,
+            `../../assets/images/ogp/gasha_item/${item.tokens[0].tokenId}.png`,
+          ),
+        );
+        const img = await sharp(itemImage).resize(96, 96).png().toBuffer();
+        return {
+          input: img,
+          left: 548,
+          top: 604 + index * 125,
+        };
+      }),
+    );
+
+    const itemsQuantityContainer = await Promise.all(
+      items.map(async (item, index) => {
+        const img = await sharp({
+          text: {
+            text: `<span foreground="black" font_weight="bold">x ${item.tokens[0].quantity}</span>`,
+            font: 'Albert Sans',
+            fontfile: fontBold,
+            rgba: true,
+            width: 100,
+            height: 23,
+          },
+        })
+          .png()
+          .toBuffer();
+        return {
+          input: img,
+          left: 660,
+          top: 642 + index * 125,
         };
       }),
     );
@@ -195,16 +216,18 @@ export class OgpService {
       {
         input: pointLabel,
         left: 50,
-        top: 357,
+        top: 360,
       },
       {
-        input: address,
+        input: addressImage,
         left: 53,
         top: 425,
       },
       ...itemsContainer,
       ...pointsContainer,
       ...pointLabelsContainer,
+      ...itemsImageContainer,
+      ...itemsQuantityContainer,
     ]);
 
     const buffer = await container.toBuffer();
@@ -218,7 +241,7 @@ export class OgpService {
 
     const container = sharp(base);
 
-    const pointNum = '20000';
+    const pointNum = '200900';
     const point = await createTextSVG(pointNum, 512, 128, blue, 'end', 80);
     const pointLabel = await createTextSVG('$BALL', 150, 64, blue, 'end', 40);
     const address = await createTextSVG(
@@ -334,5 +357,22 @@ export class OgpService {
 
     const buffer = await container.toBuffer();
     return buffer;
+  }
+
+  async getResult(id: number) {
+    const res = await this.scorecardRepository.findOne({ where: { id } });
+
+    return res;
+  }
+
+  async saveResult(address: string, result: any[]) {
+    // ここで結果を保存する処理を書く
+    const res = await this.scorecardRepository.save({
+      address,
+      result,
+      date: Math.ceil(new Date().getTime() / 1000),
+    });
+
+    return res;
   }
 }
