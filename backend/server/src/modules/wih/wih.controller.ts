bb4fd517819bf6dfb92ed9d29d51011f0a4ac3ff
kunroku <es-shirahama+shirahama@eversystem.co.jp>
2024-04-19 17:28:00 +0900

    fix contract call args

--- backend/server/src/modules/wih/wih.controller.ts
+++ backend/server/src/modules/wih/wih.controller.ts
@@ -1,7 +1,6 @@
 import {
   Controller,
   Post,
-  Get,
   Body,
   Logger,
   Param,
@@ -30,8 +29,9 @@ export class WIHController {
     const address =
       req.action.signer.client.verified_addresses.eth_addresses[0];
     const { count } = await this.wihService.getWIHCount(address);
-    this.wihService.createHat(address, count);
-    this.wihService.incrementWIHCount(address);
+    const hat = this.wihService.createHat(address, count);
+    const hash = hat.hash.map((e) => Buffer.from(e).toString('hex'));
+    return { options: hash };
   }
 
   @Post('/choosehat/:options')
@@ -51,69 +51,37 @@ export class WIHController {
     if (count !== wihcount.count) {
       throw new BadRequestException('bad hat count');
     }
+    await this.wihService.incrementWIHCount(address);
     const hat = this.wihService.createHat(address, count);
-    if (hat.selected === buttonIndex) {
-      const selection = hat.selection;
-      const pubkey = Buffer.from(hat.pubkey).toString('hex');
-      const signature = hat.signature.map((e) =>
-        Buffer.from(e).toString('hex'),
-      );
-      const hash = hat.hash.map((e) => Buffer.from(e).toString('hex'));
-      const proof = JSON.stringify({ selection, pubkey, signature, hash });
-      await this.wihService.incrementWIHCount(address);
-      await this.syndicateService.sendTransaction(
-        FORWARDER_ADDRESS,
-        666666666,
-        'burnAndRedeemReward(address to, uint256 rewardValue, string memo)',
-        { to: address, rewardValue: hat.reward, memo: proof },
-      );
-    } else {
-      await this.syndicateService.sendTransaction(
-        FORWARDER_ADDRESS,
-        666666666,
-        'burnAndRedeemLostNFT(address to)',
-        { to: address },
-      );
-    }
-  }
-
-  @Get('/debug/:count')
-  async chooseHatDebug(@Param('count') count: string) {
-    const countnumber = Number(count);
-    const address = '0xD0575cA24D907b35d39383a53c3300D510446BaE'.toLowerCase();
-    const wihcount = await this.wihService.getWIHCount(address);
-    if (wihcount.count !== countnumber) {
-      throw new BadRequestException('bad hat number');
-    }
-    const hat = this.wihService.createHat(address, countnumber);
-    const selected = 0;
     const selection = hat.selection;
     const pubkey = Buffer.from(hat.pubkey).toString('hex');
     const signature = hat.signature.map((e) =>
       Buffer.from(e).toString('hex'),
     );
     const hash = hat.hash.map((e) => Buffer.from(e).toString('hex'));
-    // const proof = Buffer.from(JSON.stringify({ selection, pubkey, signature, hash })).toString('hex');
     const proof = JSON.stringify({ selection, pubkey, signature, hash });
-    this.logger.debug(proof);
-    if (hat.selected === selected) {
-      await this.wihService.incrementWIHCount(address);
-      const res = await this.syndicateService.sendTransaction(
+    if (hat.selected === buttonIndex) {
+      await this.syndicateService.sendTransaction(
         FORWARDER_ADDRESS,
         666666666,
         'burnAndRedeemReward(address to, uint256 rewardValue, string memo)',
-        { to: address, rewardValue: hat.reward, memo: proof },
+        { to: address, rewardValue: hat.reward.value, memo: proof },
       );
-      this.logger.debug(res);
+      return {
+        hit: true,
+        degen_amount: hat.reward.value,
+      }
     } else {
-      await this.wihService.incrementWIHCount(address);
-      const res = await this.syndicateService.sendTransaction(
+      await this.syndicateService.sendTransaction(
         FORWARDER_ADDRESS,
         666666666,
-        'burnAndRedeemLostNFT(address to)',
-        { to: address },
+        'burnAndRedeemLostNFT(address to, string memo)',
+        { to: address, memo: proof },
       );
-      this.logger.debug(res);
+      return {
+        hit: false,
+        degen_amount: 0,
+      }
     }
   }
 }
